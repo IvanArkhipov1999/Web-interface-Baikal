985b64db9e3e8d6eabcea6a59ac321219c6d696c
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2022-03-18 12:50:33 +0300

    compiling

---
+++ scripts/compile.php
@@ -0,0 +1,11 @@
+<?php
+function compile()
+{
+	$output = shell_exec('clang-11 --target=mipsel-linux-gnu -static executable_code/code.c -o executable_code/code -lm');
+
+	return $output;
+}
+
+compile();
+?>
+
